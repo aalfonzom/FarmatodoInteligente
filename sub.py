ae831c5d7f1d91d62b63f5622d01cd0694cf447e
Ricardo Gomez <ricardogb0902@ggmail.com>
2019-10-20 11:25:00 -0400

    Creando pub y sub

---
+++ sub.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import time
+import datetime
+
+client = mqtt.Client()
+broker = "localhost"
+topic = "Farmatodo/Sedes/Camara/#"
+QOS = 0
+date = datetime.datetime.now()
+
+def on_connect(client, userdata, flags, rc):
+    if(rc == 0):
+        print("Se ha conectado al Cliente")
+        rc = client.subscribe(topic, QOS)
+
+def on_message(client, userdata, msg):
+    print("\n---------------------------------------------------")
+    print("Mensaje         : " +str(msg.payload.decode()))
+    print("Qos del mensaje : "+str(msg.qos))
+    print("Tópico          : "+str(msg.topic))
+    print("Hora del mensaje: "+str(date))
+    print("---------------------------------------------------\n")
+
+client.on_connect   =   on_connect
+client.on_message = on_message
+
+client.connect(broker)
+
+time.sleep(2)
+client.loop_forever()
